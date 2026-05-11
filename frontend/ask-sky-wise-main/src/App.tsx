d7025d2c0d2e3dec620a3905c326f9bb9195cc7a
Kadir Gokdeniz <kadirqokdeniz@hotmail.com>
2026-05-11 13:17:32 +0300

    feat: set base path to /global_gate

--- frontend/ask-sky-wise-main/src/App.tsx
+++ frontend/ask-sky-wise-main/src/App.tsx
@@ -37,7 +37,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/global_gate">
           <ErrorBoundary onError={handleErrorBoundaryError}>
             <BackgroundFX />
             <div className="relative z-10">
